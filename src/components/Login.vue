8a015a35ac66646f57ef887e8f4698c9a9972733
ifer-itcast <yangkangkang@itcast.cn>
2020-02-26 20:53:18 +0800

    表单的重置功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
         <img src="../assets/logo.png" alt="">
       </div>
       <!-- 登录表单区域 -->
-      <el-form class="login_form" :model="loginForm" :rules="loginFormRules">
+      <el-form ref="loginFormRef" class="login_form" :model="loginForm" :rules="loginFormRules">
         <!-- 用户名 -->
         <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
@@ -18,7 +18,7 @@
         <!-- 按钮 -->
         <el-form-item class="btns">
           <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -59,6 +59,12 @@ export default {
           trigger: 'blur'
         }]
       }
+    }
+  },
+  methods: {
+    // 重置登录表单
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
     }
   }
 }
