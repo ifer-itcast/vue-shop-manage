f37826a4c3d4b0e4721acf301ccacf985eb819aa
ifer-itcast <yangkangkang@itcast.cn>
2020-02-26 20:57:22 +0800

    登录提交前的预验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,7 +17,7 @@
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
@@ -65,6 +65,11 @@ export default {
     // 重置登录表单
     resetLoginForm () {
       this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        console.log(valid)
+      })
     }
   }
 }
