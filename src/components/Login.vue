cc48bcfc2449a7711818eacc9c17851211e0efca
ifer-itcast <yangkangkang@itcast.cn>
2020-02-26 20:31:15 +0800

    使用iconfont为文本框添加图标

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,11 +9,11 @@
       <el-form class="login_form">
         <!-- 用户名 -->
         <el-form-item>
-          <el-input></el-input>
+          <el-input prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
         <el-form-item>
-          <el-input></el-input>
+          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
