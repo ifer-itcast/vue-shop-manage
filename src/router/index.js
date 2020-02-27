69c6945f8f7798ef507b9c95906c8ddf7c7651a7
ifer-itcast <yangkangkang@itcast.cn>
2020-02-27 18:15:32 +0800

    路由导航守卫控制页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -19,4 +19,17 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  // to 要访问的路径
+  // from 从哪里来的
+  // next() 直接放行，next('/login') 表示跳转
+  // 要访问 /login 的话那直接放行
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  // token 不存在那就跳转到登录页面
+  if (!tokenStr) return next('/login')
+  // 否则 token 存在那就放行
+  next()
+})
+
 export default router
