dbe22a89531da8f7ae48d2452800dd955c8a01d9
ifer-itcast <yangkangkang@itcast.cn>
2020-02-27 19:04:33 +0800

    Header结构及样式调整

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,10 @@
   <el-container class="home-container">
     <!-- 头部 -->
     <el-header>
-      Header
+      <div>
+        <img src="../assets/heima.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!-- 页面主体 -->
@@ -31,7 +34,20 @@ export default {
   height: 100%;
 }
 .el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: #373d41;
+  padding-left: 0;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 .el-aside {
   background-color: #333744;
