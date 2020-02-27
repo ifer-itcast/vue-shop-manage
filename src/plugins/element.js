72758d2758c21fca46d393dc6d906e27abd515b1
ifer-itcast <yangkangkang@itcast.cn>
2020-02-27 18:56:29 +0800

    Home页面基本布局

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Header, Container, Aside, Main } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
 
 // 把弹框组件挂着到了 vue 的原型对象上，这样每一个组件都可以直接通过 this 访问
 Vue.prototype.$message = Message
