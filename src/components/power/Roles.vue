95608c889e6cd9ac072d5a32abe764f4b402b726
ifer-itcast <yangkangkang@itcast.cn>
2020-03-06 07:24:02 +0800

    获取角色列表数据

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -1,11 +1,44 @@
 <template>
-    <div>
-        角色列表
-    </div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- 添加角色按钮 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 所有角色列表数据
+      rolelist: []
+    }
+  },
+  created() {
+    this.getRolesList()
+  },
+  methods: {
+    // 获取所有角色的列表
+    async getRolesList() {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.rolelist = res.data
+    }
+  }
 }
 </script>
 
