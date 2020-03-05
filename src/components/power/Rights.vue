f99967a768772c34634362800cacebd4c5425f5c
ifer-itcast <yangkangkang@itcast.cn>
2020-03-06 07:04:08 +0800

    获取权限列表数据

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -1,11 +1,39 @@
 <template>
-    <div>
-        <h3>权限列表组价</h3>
-    </div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      123
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 权限列表
+      rightsList: []
+    }
+  },
+  created() {
+    // 获取所有的权限
+    this.getRightsList()
+  },
+  methods: {
+    async getRightsList() {
+      const { data: res } = await this.$http.get('rights/list')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取权限列表失败')
+      }
+      this.rightsList = res.data
+    }
+  }
 }
 </script>
 
