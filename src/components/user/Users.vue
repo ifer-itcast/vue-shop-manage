4b4aa3d80a8523b50194e3469a122d3e2508ea9b
ifer-itcast <yangkangkang@itcast.cn>
2020-03-05 18:59:16 +0800

    对话框关闭后重置表单

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -57,7 +57,7 @@
       ></el-pagination>
     </el-card>
     <!-- 添加用户对话框 -->
-    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="30%">
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="30%" @close="addDialogClosed">
       <!-- 内容主体 -->
       <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
         <el-form-item label="用户名" prop="username">
@@ -189,6 +189,10 @@ export default {
         return this.$message.error('更新用户状态失败')
       }
       this.$message.success('更新用户状态成功')
+    },
+    // 监听添加用户对话框的关闭事件
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
     }
   }
 }
