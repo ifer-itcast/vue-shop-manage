cb2c4f1ddccd431efb002bf2c475180a372cc59c
ifer-itcast <yangkangkang@itcast.cn>
2020-03-06 19:54:26 +0800

    第一层for循环渲染一级权限

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -17,7 +17,18 @@
       <!-- 角色列表区域 -->
       <el-table :data="rolelist" border stripe>
         <!-- 展开列 -->
-        <el-table-column type="expand"></el-table-column>
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row v-for="(item1) in scope.row.children" :key="item1.id">
+              <!-- 渲染一级权限 -->
+              <el-col :span="5">
+                <el-tag>{{item1.authName}}</el-tag>
+              </el-col>
+              <!-- 渲染二级和三级权限 -->
+              <el-col :span="19"></el-col>
+            </el-row>
+          </template>
+        </el-table-column>
         <!-- 索引列 -->
         <el-table-column type="index"></el-table-column>
         <el-table-column label="角色名称" prop="roleName"></el-table-column>
