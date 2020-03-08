18677b0edd1d8915418085f66bfd0f86bab341eb
ifer-itcast <yangkangkang@itcast.cn>
2020-03-08 10:07:32 +0800

    配置商品分类组件路由规则

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>商品分类</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
