b164017e46468ecb3d93409925a912a60c79c443
ifer-itcast <yangkangkang@itcast.cn>
2020-03-08 10:12:00 +0800

    绘制商品分类组件基本布局

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>商品分类</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格区域 -->
+      <!-- 分页区域 -->
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -7,12 +24,10 @@ export default {
   data() {
     return {}
   },
-  created() {
-  },
+  created() {},
   methods: {}
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
