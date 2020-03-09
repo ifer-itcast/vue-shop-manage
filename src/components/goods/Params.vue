01a2b0ca4d93facbe2b97a5019522fbf825f9bca
ifer-itcast <yangkangkang@itcast.cn>
2020-03-09 12:35:52 +0800

    渲染商品分类下选择商品分类UI

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 警告区域 -->
+      <el-alert :closable="false" title="注意：只允许为第三级分类设置相关参数！" type="warning" show-icon></el-alert>
+      <!-- 选择商品分类区域 -->
+      <el-row class="cat_opt">
+        <el-col>
+          <span>选择商品分类：</span>
+          <!-- 选择商品分类的级联选择框 -->
+          <el-cascader
+            expand-trigger="hover"
+            :options="catelist"
+            :props="cateProps"
+            v-model="selectedCateKeys"
+            @change="handleChange"
+          ></el-cascader>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      catelist: [],
+      // 级联选择框的配置对象
+      cateProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      },
+      // 级联选择框双向绑定到的数组
+      selectedCateKeys: []
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败')
+      }
+      // 获取商品分类成功
+      this.catelist = res.data
+    },
+    // 级联选择框选中项变化会触发
+    handleChange() {
+      if (this.selectedCateKeys.length !== 3) {
+        // 证明选中的不是 3 级分类
+        this.selectedCateKeys = []
+        return false
+      }
+      // 选中的是 3 级分类
+      console.log(this.selectedCateKeys)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.cat_opt {
+  margin: 15px 0;
+}
+</style>
