4983a27e33cd94fdabda65fe3df200cad66a81e7
ifer-itcast <yangkangkang@itcast.cn>
2020-03-16 09:53:59 +0800

    商品列表的分页、查询、删除功能

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -10,8 +10,8 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getGoodsList">
+            <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
@@ -28,10 +28,15 @@
           <template slot-scope="scope">{{scope.row.add_time | dateFormat}}</template>
         </el-table-column>
         <el-table-column label="操作" width="130">
-          <template slot-scope>
+          <template slot-scope="scope">
             <div>
               <el-button type="primary" size="mini" icon="el-icon-edit"></el-button>
-              <el-button type="danger" size="mini" icon="el-icon-delete"></el-button>
+              <el-button
+                type="danger"
+                size="mini"
+                icon="el-icon-delete"
+                @click="removeById(scope.row.goods_id)"
+              ></el-button>
             </div>
           </template>
         </el-table-column>
@@ -89,6 +94,24 @@ export default {
     },
     handleCurrentChange(newPage) {
       this.queryInfo.pagenum = newPage
+      this.getGoodsList()
+    },
+    async removeById(id) {
+      const confirmResult = await this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
+
+      const { data: res } = await this.$http.delete(`goods/${id}`)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除失败')
+      }
+      this.$message.success('删除成功')
       this.getGoodsList()
     }
   }
