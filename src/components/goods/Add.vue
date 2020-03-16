ddc8a00dc38465cceb8f61f59ce5497aaa70443c
ifer-itcast <yangkangkang@itcast.cn>
2020-03-16 10:21:07 +0800

    实现步骤条与Tabs栏的联动效果

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -11,11 +11,22 @@
       <!-- 提示区域 -->
       <el-alert title="添加商品信息" type="info" center show-icon closable="false"></el-alert>
       <!-- 步骤条区域 -->
-      <el-steps :space="200" :active="1" finish-status="success">
-        <el-step title="已完成"></el-step>
-        <el-step title="进行中"></el-step>
-        <el-step title="步骤 3"></el-step>
+      <el-steps :space="200" :active="activeIndex-0" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
       </el-steps>
+      <!-- tab 栏区域 -->
+      <el-tabs :tab-position="tabPosition" style="height: 200px;" v-model="activeIndex">
+        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
@@ -23,7 +34,10 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      activeIndex: '0',
+      tabPosition: 'left'
+    }
   },
   created() {},
   methods: {}
